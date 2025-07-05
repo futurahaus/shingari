b46e83b4a45176c091909e677b8edcc5d182c222
Fernando Mariscotti <fernandomariscotti@MacBook-Pro-de-Fernando.local>
2025-07-05 18:35:00 -0300

    ajuste url images

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'kurswpuvjyhgojdkpxkn.supabase.co',
+        hostname: 'spozhuqlvmaieeqtaxvq.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
