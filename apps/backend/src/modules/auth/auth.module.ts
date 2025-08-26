f53175e2d6e472b7d87e29380c76366bd911124e
Fernando Mariscotti <fernandomariscotti@192.168.1.6>
2025-08-26 11:20:56 -0300

    Tiempo de access token

--- apps/backend/src/modules/auth/auth.module.ts
+++ apps/backend/src/modules/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { DatabaseModule } from '../database/database.module';
         PassportModule,
         JwtModule.register({
             secret: process.env.JWT_ACCESS_SECRET || '',
-            signOptions: { expiresIn: '15m' },
+            signOptions: { expiresIn: '1d' },
         }),
         DatabaseModule,
     ],
