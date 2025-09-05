ea19dbb56492179398d6aeaaf329fd356051b0ff
Lautaro Bipe <bipelautaro@gmail.com>
2025-09-05 19:50:02 -0300

    fix: add mailservice to rewards module.

--- apps/backend/src/modules/rewards/rewards.module.ts
+++ apps/backend/src/modules/rewards/rewards.module.ts
@@ -3,9 +3,10 @@ import { RewardsService } from './rewards.service';
 import { RewardsController } from './rewards.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { DatabaseModule } from '../database/database.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [PrismaModule, DatabaseModule],
+  imports: [PrismaModule, DatabaseModule, MailModule],
   controllers: [RewardsController],
   providers: [RewardsService],
   exports: [RewardsService],
