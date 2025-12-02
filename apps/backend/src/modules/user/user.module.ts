57bffd2939f788d510aab44db55fe029a11f550b
joss-futura <jose@futura.haus>
2025-12-02 11:11:57 -0300

    feat(MKT-677): auto-add product to favorites when special price is assigned

--- apps/backend/src/modules/user/user.module.ts
+++ apps/backend/src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { OrdersModule } from '../orders/orders.module';
+import { FavoritesModule } from '../favorites/favorites.module';
 
 @Module({
-  imports: [PrismaModule, OrdersModule],
+  imports: [PrismaModule, OrdersModule, FavoritesModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
